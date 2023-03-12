b47598d235d4e765a5ea6be15d60f81b95376709
primary-scholar <primaryscholar@126.com>
2023-03-12 17:12:32 +0800

    feat(add agorithm): add agorithm
    add agorithm

--- simple-java/src/main/java/com/mimu/simple/java/algorithm/AlgorithmRelevant.java
+++ simple-java/src/main/java/com/mimu/simple/java/algorithm/AlgorithmRelevant.java
@@ -3,11 +3,12 @@ package com.mimu.simple.java.algorithm;
 /**
  * 算法复杂度排序
  * O(1)     算术操作,寻址操作，移位操作
- * O(logN)  链表数据查询
- * O(N)
+ * O(logN)  二分查找
+ * O(N)  链表数据查询
  * O(N*logN)
  * O(N^2)  O(N^3) ... O(N^)
  * O(2^N) O(3^N) ...
+ * O(N!)
  */
 public class AlgorithmRelevant {
 }
