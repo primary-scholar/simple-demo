e689ad52b9d36523e5ef28dfbe9550e92ddfdca2
primary-scholar <primaryscholar@126.com>
2023-03-12 10:30:08 +0800

    feat(add algorithm relevant): add algorithm relevant
    add algorithm relevant

--- simple-java/src/main/java/com/mimu/simple/java/algorithm/ClassSort.java
+++ simple-java/src/main/java/com/mimu/simple/java/algorithm/ClassSort.java
@@ -1,7 +1,34 @@
 package com.mimu.simple.java.algorithm;
 
+import java.util.Objects;
+
 /**
  * 几种经典算法
  */
 public class ClassSort {
+
+    private void swap(Integer[] array, int i, int j) {
+        if (Objects.isNull(array) || array.length <= 1) {
+            return;
+        }
+        Integer tmp = array[i];
+        array[i] = array[j];
+        array[j] = tmp;
+    }
+
+    public void bubbleSort(Integer[] array) {
+        if (Objects.isNull(array) || array.length < 1) {
+            return;
+        }
+        // 0~n-1 次排序
+        for (int i = 0; i < array.length; i++) {
+            // 外层每循环一次 则就有一个元素排在了合适位置 所以这里需要 -i；-1 是防止内循环数组越界
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    //相邻两个元素比较交换
+                    swap(array, j, j + 1);
+                }
+            }
+        }
+    }
 }
---
+++ simple-java/src/test/java/com/mimu/simple/java/algorithm/ClassSortTest.java
@@ -0,0 +1,40 @@
+package com.mimu.simple.java.algorithm;
+
+import com.alibaba.fastjson.JSONObject;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+
+public class ClassSortTest {
+
+    public ClassSort sort = new ClassSort();
+
+    public Integer[] initArray() {
+        int arrayLength = (int) (Math.random() * 19) + 1;
+        Integer[] array = new Integer[arrayLength];
+        for (int i = 0; i < arrayLength; i++) {
+            array[i] = (int) (Math.random() * 100) - (int) (Math.random() * 100);
+        }
+        return array;
+    }
+
+    public Integer[] copyArray(Integer array[]) {
+        return Arrays.copyOf(array, array.length);
+    }
+
+    @Test
+    public void bubbleSort() {
+        int loop = (int) (Math.random() * 499) + 1;
+        for (int l = 0; l < loop; l++) {
+            Integer[] array = initArray();
+            Integer[] copy = copyArray(array);
+            sort.bubbleSort(array);
+            Arrays.sort(copy);
+            if (!Arrays.equals(array, copy)) {
+                System.out.println(JSONObject.toJSONString(array));
+                System.out.println(JSONObject.toJSONString(copy));
+            }
+        }
+    }
+}
