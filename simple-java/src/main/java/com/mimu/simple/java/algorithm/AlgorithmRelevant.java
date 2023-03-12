0b2ff15387d22ac66d9babda5e189756ed681c74
primary-scholar <primaryscholar@126.com>
2023-03-12 09:18:04 +0800

    feat(add algorithm relevant): add algorithm relevant
    add algorithm relevant

---
+++ simple-java/src/main/java/com/mimu/simple/java/algorithm/AlgorithmRelevant.java
@@ -0,0 +1,13 @@
+package com.mimu.simple.java.algorithm;
+
+/**
+ * 算法复杂度排序
+ * O(1)     算术操作,寻址操作，移位操作
+ * O(logN)  链表数据查询
+ * O(N)
+ * O(N*logN)
+ * O(N^2)  O(N^3) ... O(N^)
+ *
+ */
+public class AlgorithmRelevant {
+}
