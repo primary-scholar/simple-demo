a11f247d89cbbd695c39fb19a9dc796667b2319b
primary-scholar <primaryscholar@126.com>
2023-03-17 00:41:51 +0800

    feat(modify agorithm model): modify agorithm model
    modify agorithm model fill ClassNodeRelevant class

--- simple-java/src/main/java/com/mimu/simple/java/algorithm/ClassicNodeRelevant.java
+++ simple-java/src/main/java/com/mimu/simple/java/algorithm/ClassicNodeRelevant.java
@@ -11,28 +11,61 @@ import java.util.Objects;
  */
 public class ClassicNodeRelevant {
 
-
     /**
      * 单链表 倒序
+     * 整体思路：在反转过程中 取 pre，head，next(临时节点-head的后继节点) 三个节点
+     * 分别表示 单链表 头节点的 前驱节点，头节点和 头节点的后继节点 初始时 pre 和 next 都为 null 即可
+     * 头节点非空 开始循环
+     * <p>
+     * 首先 给 next 即 head 的后继节点赋值，
+     * 然后 头节点 的 next 赋值为 前驱 pre 节点，
+     * pre 节点后移即当前的 head 节点 ，
+     * head 头节点继续后移 即当前的 next 节点
+     * <p>
+     * 然后 无限循环知道 头节点为空；
      *
      * @param head
      * @return
      */
-    public OneLinkNode<Integer> oneLinkNodeRevert(OneLinkNode<Integer> head) {
-        if (Objects.isNull(head)) {
-            return null;
+    public SingleNodeList<Integer> revertSingleNodeList(SingleNodeList<Integer> head) {
+        SingleNodeList<Integer> pre = null;
+        SingleNodeList<Integer> next;
+        while (Objects.nonNull(head)) {
+            next = head.getNext();
+            head.setNext(pre);
+            pre = head;
+            head = next;
         }
-        OneLinkNode<Integer> curr = head;
-        OneLinkNode<Integer> next = head.getNext();
-        while (Objects.nonNull(next)) {
-            OneLinkNode<Integer> temp = next.getNext();
-            next.setNext(curr);
-            curr = next;
-            next = temp;
+        return pre;
+    }
 
+    /**
+     * 双链表 倒序  和单链表类似
+     * 整体思路：在反转过程中 取 pre，head，next(临时节点-head的后继节点) 三个节点
+     * 分别表示 单链表 头节点的 前驱节点，头节点和 头节点的后继节点 初始时 pre 和 next 都为 null 即可
+     * 头节点非空 开始循环
+     * <p>
+     * 首先 给 next 即 head 的后继节点赋值，
+     * 然后 头节点 的 next 赋值为 前驱 pre 节点， 头节点的 pre 赋值为 后继 next 节点
+     * pre 节点后移即当前的 head 节点 ，
+     * head 节点继续后即当前的 next 节点
+     * <p>
+     * 然后 无限循环知道 头节点为空；
+     *
+     * @param head
+     * @return
+     */
+    public DoubleNodeList<Integer> revertDoubleNodeList(DoubleNodeList<Integer> head) {
+        DoubleNodeList<Integer> pre = null;
+        DoubleNodeList<Integer> next;
+        while (Objects.nonNull(head)) {
+            next = head.getNext();
+            head.setNext(pre);
+            head.setPre(next);
+            pre = head;
+            head = next;
         }
-        head.setNext(null);
-        return curr;
+        return pre;
     }
 
 
@@ -43,30 +76,29 @@ public class ClassicNodeRelevant {
      * @param delete
      * @return
      */
-    public OneLinkNode<Integer> oneLinkNodeDelete(OneLinkNode<Integer> head, Integer delete) {
+    public SingleNodeList<Integer> deleteSingleNodeList(SingleNodeList<Integer> head, Integer delete) {
         if (Objects.isNull(head)) {
             return null;
         }
-        OneLinkNode<Integer> curr = head;
-        while (Objects.nonNull(curr) && curr.getData().equals(delete)) {
-            OneLinkNode<Integer> tmp = curr.getNext();
-            curr.setNext(null);
-            curr = tmp;
+        while (Objects.nonNull(head) && head.getData().equals(delete)) {
+            SingleNodeList<Integer> next = head.getNext();
+            head.setNext(null);
+            head = next;
         }
-        if (Objects.isNull(curr)) {
-            return null;
-        }
-        head = curr;
-        OneLinkNode<Integer> next = curr.getNext();
-        while (Objects.nonNull(next)) {
-            OneLinkNode<Integer> tmp = next.getNext();
-            if (next.getData().equals(delete)) {
-                next.setNext(null);
-                curr.setNext(tmp);
-            } else {
-                curr = next;
+        if (Objects.nonNull(head)) {
+            SingleNodeList<Integer> curr = head;
+            SingleNodeList<Integer> next = curr.getNext();
+            while (Objects.nonNull(next)) {
+                if (next.getData().equals(delete)) {
+                    SingleNodeList<Integer> tmp = next;
+                    next = next.getNext();
+                    curr.setNext(next);
+                    tmp.setNext(null);
+                } else {
+                    curr = next;
+                    next = next.getNext();
+                }
             }
-            next = tmp;
         }
         return head;
     }
@@ -77,7 +109,7 @@ public class ClassicNodeRelevant {
      * @param node
      * @return
      */
-    public TwoLinkNode<Integer> twoLinkNodeRevert(TwoLinkNode<Integer> node) {
+    public DoubleNodeList<Integer> twoLinkNodeRevert(DoubleNodeList<Integer> node) {
         return null;
     }
 
@@ -88,7 +120,7 @@ public class ClassicNodeRelevant {
      * @param delete
      * @return
      */
-    public TwoLinkNode<Integer> twoLinkNodeDelete(TwoLinkNode<Integer> node, Integer delete) {
+    public DoubleNodeList<Integer> twoLinkNodeDelete(DoubleNodeList<Integer> node, Integer delete) {
         return null;
     }
 
@@ -98,9 +130,9 @@ public class ClassicNodeRelevant {
      */
     @Setter
     @Getter
-    public static class OneLinkNode<T> {
+    public static class SingleNodeList<T> {
         private T data;
-        private OneLinkNode<T> next;
+        private SingleNodeList<T> next;
     }
 
     /**
@@ -108,9 +140,9 @@ public class ClassicNodeRelevant {
      */
     @Setter
     @Getter
-    public static class TwoLinkNode<T> {
+    public static class DoubleNodeList<T> {
         private T data;
-        private TwoLinkNode<T> pre;
-        private TwoLinkNode<T> next;
+        private DoubleNodeList<T> pre;
+        private DoubleNodeList<T> next;
     }
 }
--- simple-java/src/test/java/com/mimu/simple/java/algorithm/ClassicNodeRelevantTest.java
+++ simple-java/src/test/java/com/mimu/simple/java/algorithm/ClassicNodeRelevantTest.java
@@ -8,22 +8,22 @@ import java.util.Objects;
 
 public class ClassicNodeRelevantTest {
     public int loop = (int) (Math.random() * 99999) + 1;
+    public int nodeLinkLength = (int) (Math.random() * 20);
     ClassicNodeRelevant nodeRelevant = new ClassicNodeRelevant();
 
-    public ClassicNodeRelevant.OneLinkNode<Integer> initOneNodeLink() {
-        int nodeLinkLength = (int) (Math.random() * 20);
-        ClassicNodeRelevant.OneLinkNode<Integer> head = null;
-        ClassicNodeRelevant.OneLinkNode<Integer> curr = null;
+    public ClassicNodeRelevant.SingleNodeList<Integer> initSingleNodeLink() {
+
+        ClassicNodeRelevant.SingleNodeList<Integer> head = null;
+        ClassicNodeRelevant.SingleNodeList<Integer> curr = null;
         for (int i = 0; i < nodeLinkLength; i++) {
             if (Objects.isNull(head)) {
-                head = new ClassicNodeRelevant.OneLinkNode<>();
+                head = new ClassicNodeRelevant.SingleNodeList<>();
                 head.setData((int) (Math.random() * 100));
                 head.setNext(null);
                 curr = head;
             } else {
-                Integer data = (int) (Math.random() * 100) + i;
-                ClassicNodeRelevant.OneLinkNode<Integer> element = new ClassicNodeRelevant.OneLinkNode<>();
-                element.setData(data);
+                ClassicNodeRelevant.SingleNodeList<Integer> element = new ClassicNodeRelevant.SingleNodeList<>();
+                element.setData((int) (Math.random() * 100) + i);
                 element.setNext(null);
                 curr.setNext(element);
                 curr = element;
@@ -32,9 +32,31 @@ public class ClassicNodeRelevantTest {
         return head;
     }
 
+    public ClassicNodeRelevant.DoubleNodeList<Integer> initDoubleNodeList() {
+        ClassicNodeRelevant.DoubleNodeList<Integer> head = null;
+        ClassicNodeRelevant.DoubleNodeList<Integer> curr = null;
+        for (int i = 0; i < nodeLinkLength; i++) {
+            if (Objects.isNull(head)) {
+                head = new ClassicNodeRelevant.DoubleNodeList<>();
+                head.setData((int) (Math.random() * 100));
+                head.setPre(null);
+                head.setNext(null);
+                curr = head;
+            } else {
+                ClassicNodeRelevant.DoubleNodeList<Integer> element = new ClassicNodeRelevant.DoubleNodeList<>();
+                curr.setNext(element);
+                element.setData((int) (Math.random() * 100) + i);
+                element.setPre(curr);
+                element.setNext(null);
+                curr = element;
+            }
+        }
+        return curr;
+    }
 
-    public void printOneNodeLink(ClassicNodeRelevant.OneLinkNode<Integer> head) {
-        ClassicNodeRelevant.OneLinkNode<Integer> curr = head;
+
+    public void printSingleNodeLink(ClassicNodeRelevant.SingleNodeList<Integer> head) {
+        ClassicNodeRelevant.SingleNodeList<Integer> curr = head;
         while (Objects.nonNull(curr)) {
             System.out.print(curr.getData());
             System.out.print("  ");
@@ -43,14 +65,24 @@ public class ClassicNodeRelevantTest {
         System.out.println("");
     }
 
-    public Boolean isExistData(ClassicNodeRelevant.OneLinkNode<Integer> head, Integer data) {
+    private void printDoubleNodeList(ClassicNodeRelevant.DoubleNodeList<Integer> head) {
+        ClassicNodeRelevant.DoubleNodeList<Integer> curr = head;
+        while (Objects.nonNull(curr)) {
+            System.out.print(curr.getData());
+            System.out.print("  ");
+            curr = curr.getNext();
+        }
+        System.out.println("");
+    }
+
+    public Boolean isExistDataInSingleNodeList(ClassicNodeRelevant.SingleNodeList<Integer> head, Integer data) {
         if (Objects.isNull(head)) {
             return Boolean.FALSE;
         }
         if (head.getData().equals(data)) {
             return Boolean.TRUE;
         }
-        ClassicNodeRelevant.OneLinkNode<Integer> next = head.getNext();
+        ClassicNodeRelevant.SingleNodeList<Integer> next = head.getNext();
         while (Objects.nonNull(next)) {
             if (next.getData().equals(data)) {
                 return Boolean.TRUE;
@@ -61,28 +93,34 @@ public class ClassicNodeRelevantTest {
         return Boolean.FALSE;
     }
 
-    public String nodeDataToString(ClassicNodeRelevant.OneLinkNode<Integer> head) {
-        if (Objects.isNull(head)) {
-            return StringUtils.EMPTY;
-        }
+    public String dataToStringWithSingleNodeList(ClassicNodeRelevant.SingleNodeList<Integer> head) {
+        ClassicNodeRelevant.SingleNodeList<Integer> curr = head;
         StringBuilder builder = new StringBuilder();
-        builder.append(head.getData());
-        ClassicNodeRelevant.OneLinkNode<Integer> next = head.getNext();
-        while (Objects.nonNull(next)) {
-            builder.append(next.getData());
-            next = next.getNext();
+        while (Objects.nonNull(curr)) {
+            builder.append(curr.getData());
+            curr = curr.getNext();
+        }
+        return builder.toString();
+    }
+
+    public String dataToStringWithDoubleNodeList(ClassicNodeRelevant.DoubleNodeList<Integer> head) {
+        ClassicNodeRelevant.DoubleNodeList<Integer> curr = head;
+        StringBuilder builder = new StringBuilder();
+        while (Objects.nonNull(curr)) {
+            builder.append(curr.getData());
+            curr = curr.getNext();
         }
         return builder.toString();
     }
 
 
     @Test
-    public void oneLinkNodeRevert() {
+    public void revertSingleNodeListTest() {
         for (int i = 0; i < loop; i++) {
-            ClassicNodeRelevant.OneLinkNode<Integer> head = initOneNodeLink();
-            ClassicNodeRelevant.OneLinkNode<Integer> origin = nodeRelevant.oneLinkNodeRevert(nodeRelevant.oneLinkNodeRevert(head));
-            if (!nodeDataToString(head).equals(nodeDataToString(origin))) {
-                printOneNodeLink(head);
+            ClassicNodeRelevant.SingleNodeList<Integer> head = initSingleNodeLink();
+            ClassicNodeRelevant.SingleNodeList<Integer> origin = nodeRelevant.revertSingleNodeList(nodeRelevant.revertSingleNodeList(head));
+            if (!dataToStringWithSingleNodeList(head).equals(dataToStringWithSingleNodeList(origin))) {
+                printSingleNodeLink(head);
                 return;
             }
         }
@@ -90,13 +128,25 @@ public class ClassicNodeRelevantTest {
     }
 
     @Test
+    public void revertDoubleNodeListTest() {
+        for (int i = 0; i < loop; i++) {
+            ClassicNodeRelevant.DoubleNodeList<Integer> head = initDoubleNodeList();
+            ClassicNodeRelevant.DoubleNodeList<Integer> origin = nodeRelevant.revertDoubleNodeList(nodeRelevant.revertDoubleNodeList(head));
+            if (!dataToStringWithDoubleNodeList(head).equals(dataToStringWithDoubleNodeList(origin))) {
+                printDoubleNodeList(head);
+                return;
+            }
+        }
+    }
+
+    @Test
     public void oneLinkNodeDelete() {
         for (int i = 0; i < loop; i++) {
-            ClassicNodeRelevant.OneLinkNode<Integer> head = initOneNodeLink();
+            ClassicNodeRelevant.SingleNodeList<Integer> head = initSingleNodeLink();
             int data = (int) (Math.random() * 100);
-            ClassicNodeRelevant.OneLinkNode<Integer> oneLinkNode = nodeRelevant.oneLinkNodeDelete(head, data);
-            if (isExistData(oneLinkNode, data)) {
-                printOneNodeLink(oneLinkNode);
+            ClassicNodeRelevant.SingleNodeList<Integer> singleNodeList = nodeRelevant.deleteSingleNodeList(head, data);
+            if (isExistDataInSingleNodeList(singleNodeList, data)) {
+                printSingleNodeLink(singleNodeList);
                 System.out.println(data);
                 return;
             }
