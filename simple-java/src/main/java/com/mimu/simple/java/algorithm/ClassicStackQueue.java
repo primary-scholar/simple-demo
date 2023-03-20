95301e10a13361523c461bbe840de2e19ee3febf
primary-scholar <primaryscholar@126.com>
2023-03-20 21:57:48 +0800

    feat(modify agorithm model): modify agorithm model
    modify agorithm model fill ClassNodeRelevant class

--- simple-java/src/main/java/com/mimu/simple/java/algorithm/ClassicStackQueue.java
+++ simple-java/src/main/java/com/mimu/simple/java/algorithm/ClassicStackQueue.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.Objects;
+import java.util.Stack;
 
 /**
  * 栈和队列 的实现方式可以有 链表 和 数组两种方式实现
@@ -248,6 +249,51 @@ public class ClassicStackQueue {
             }
             num--;
             return data;
+        }
+
+    }
+
+
+    /**
+     * 两个栈实现队列的操作方式
+     *
+     * @param <T>
+     */
+    public static class TwoStackQueue<T> {
+        private Stack<T> pushStack;
+        private Stack<T> popStack;
+
+        public TwoStackQueue() {
+            pushStack = new Stack<>();
+            popStack = new Stack<>();
+        }
+
+        public Boolean enQueue(T data) {
+            pushStack.push(data);
+            pushToPop();
+            return Boolean.TRUE;
+        }
+
+        public T deQueue() {
+            if (pushStack.empty() && popStack.empty()) {
+                return null;
+            }
+            pushToPop();
+            return popStack.pop();
+        }
+
+
+        /**
+         * 从 pushStack 向 popStack 栈倒入数据时 必须保证
+         * 1.只有 popStack 为空时 在可以倒入数据
+         * 2.每次倒入数据时 需要把 pushStack 中的数据全部 倒进去
+         */
+        private void pushToPop() {
+            if (popStack.empty()) {
+                while (!pushStack.empty()) {
+                    popStack.push(pushStack.pop());
+                }
+            }
         }
 
     }
--- simple-java/src/test/java/com/mimu/simple/java/algorithm/ClassicStackQueueTest.java
+++ simple-java/src/test/java/com/mimu/simple/java/algorithm/ClassicStackQueueTest.java
@@ -62,7 +62,7 @@ public class ClassicStackQueueTest {
     }
 
     @Test
-    public void linkStackTest(){
+    public void linkStackTest() {
         for (int i = 0; i < capacity; i++) {
             Boolean put = linkStack.put((int) (Math.random() * 100) + i);
             assert put.equals(Boolean.TRUE);
@@ -76,5 +76,32 @@ public class ClassicStackQueueTest {
         Integer integer = linkStack.pop();
         assert Objects.isNull(integer);
     }
+
+    /**
+     * 栈实现队列
+     * <p>
+     * 使用两个栈 把其中一个栈中的元素 全量倒入到两一个栈中 倒的过程依然遵循 栈的 后进先出操作
+     */
+    @Test
+    public void stackImplementQueue() {
+        ClassicStackQueue.TwoStackQueue<Integer> stackQueue = new ClassicStackQueue.TwoStackQueue<>();
+        stackQueue.enQueue(1);
+        stackQueue.enQueue(2);
+        System.out.println(stackQueue.deQueue());
+        System.out.println(stackQueue.deQueue());
+
+    }
+
+    /**
+     * 队列实现栈
+     * <p>
+     * 使用两个队列 来回把其中一个队列中的数据(除最后一个)导入到其中一个队列中 倒的过程依然遵循 队列的先进先出原则
+     */
+    @Test
+    public void queueImplStack() {
+    }
+
+
+
 
 }
