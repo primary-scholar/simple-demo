9fa8daa6940b36e548373ba09d20f4a6e97006ea
primary-scholar <primaryscholar@126.com>
2023-05-17 23:40:51 +0800

    feat(add rocketmq model): add rocketmq model
    add rocketmq model

--- simple-common/rocketmq-demo/src/test/java/com/mimu/simple/comn/rocketmq/SimpleRocketMQNote.java
+++ simple-common/rocketmq-demo/src/test/java/com/mimu/simple/comn/rocketmq/SimpleRocketMQNote.java
@@ -1,9 +1,7 @@
 package com.mimu.simple.comn.rocketmq;
 
 /**
- * 单机rocketmq 启动说明 @see <a href="https://github.com/apache/rocketmq">...</a>
- * 1.启动 name server
- * 2.启动 broker
+ * 单机rocketmq 启动说明 ～/readme/rocket-mq.txt
  */
 public class SimpleRocketMQNote {
 }
