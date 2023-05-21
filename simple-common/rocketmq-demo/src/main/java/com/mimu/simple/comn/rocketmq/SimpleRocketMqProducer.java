05e41ddfad2e3ceaee4b08753a03ee8b404b5e02
primary-scholar <primaryscholar@126.com>
2023-05-21 21:17:46 +0800

    feat(add rocketmq model): add rocketmq model
    add rocketmq model

--- simple-common/rocketmq-demo/src/main/java/com/mimu/simple/comn/rocketmq/SimpleRocketMqProducer.java
+++ simple-common/rocketmq-demo/src/main/java/com/mimu/simple/comn/rocketmq/SimpleRocketMqProducer.java
@@ -1,16 +1,41 @@
 package com.mimu.simple.comn.rocketmq;
 
+import org.apache.rocketmq.client.exception.MQBrokerException;
+import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.client.producer.SendStatus;
 import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.exception.RemotingException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 public class SimpleRocketMqProducer {
+    private static final Logger logger = LoggerFactory.getLogger(SimpleRocketMqProducer.class);
 
-    public static void main(String[] args) {
-        DefaultMQProducer producer = new DefaultMQProducer("");
+    private DefaultMQProducer mqProducer;
+
+    public SimpleRocketMqProducer() {
+        mqProducer = new DefaultMQProducer("");
+    }
+
+    public SimpleRocketMqProducer(String producerGroup) {
+        mqProducer = new DefaultMQProducer(producerGroup);
+    }
+
+    public SendStatus send(String topic, String msg) {
+        return send(topic, null, msg);
+    }
+
+    public SendStatus send(String topic, String tag, String msg) {
+        logger.info("SimpleRocketMqProducer send msg topic={},tag={},msg={}", topic, tag, msg);
         try {
-            producer.start();
-            producer.send(new Message("topic", "tag", "message".getBytes()));
-        } catch (Exception e) {
+            mqProducer.start();
+            SendResult result = mqProducer.send(new Message(topic, tag, msg.getBytes()));
+            return result.getSendStatus();
+        } catch (MQClientException | RemotingException | MQBrokerException | InterruptedException e) {
+            logger.info("SimpleRocketMqProducer send msg topic={},tag={},msg={} error", topic, tag, msg, e);
             throw new RuntimeException(e);
         }
     }
--- simple-common/rocketmq-demo/src/test/java/com/mimu/simple/comn/rocketmq/SimpleRocketMQNote.java
+++ simple-common/rocketmq-demo/src/test/java/com/mimu/simple/comn/rocketmq/SimpleRocketMQNote.java
@@ -1,7 +1,13 @@
 package com.mimu.simple.comn.rocketmq;
 
+
 /**
  * 单机rocketmq 启动说明 ～/readme/rocket-mq.txt
  */
 public class SimpleRocketMQNote {
+
+
+    public Boolean send(String topic, String msg) {
+        return Boolean.FALSE;
+    }
 }
