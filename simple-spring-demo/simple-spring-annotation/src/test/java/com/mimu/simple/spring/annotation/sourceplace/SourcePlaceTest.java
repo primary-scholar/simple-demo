916faede9bca12d4f07ce0515febeba4547894c2
luyahui <primaryscholar@126.com>
2025-12-17 14:13:19 +0800

    format

--- simple-spring-demo/simple-spring-annotation/src/test/java/com/mimu/simple/spring/annotation/sourceplace/SourcePlaceTest.java
+++ simple-spring-demo/simple-spring-annotation/src/test/java/com/mimu/simple/spring/annotation/sourceplace/SourcePlaceTest.java
@@ -4,6 +4,8 @@ import com.mimu.simple.spring.annotation.sourceplace.config.SourceConfig;
 import com.mimu.simple.spring.annotation.sourceplace.model.DomainA;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 /**
@@ -13,10 +15,13 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 @Slf4j
 public class SourcePlaceTest {
+    private static  final Logger serverLogger = LoggerFactory.getLogger("serverLogger");
 
     @Test
     public void info() {
         AnnotationConfigApplicationContext configApplicationContext = new AnnotationConfigApplicationContext(SourceConfig.class);
         log.info("bean {}", configApplicationContext.getBean(DomainA.class));
+        serverLogger.info("bean {}", configApplicationContext.getBean(DomainA.class));
+        log.error("bean {}", configApplicationContext.getBean(DomainA.class));
     }
 }
--- simple-spring-demo/simple-spring-mybatis-demo/src/main/java/com/mimu/simple/spring/mybatis/config/DataSourceConfig.java
+++ simple-spring-demo/simple-spring-mybatis-demo/src/main/java/com/mimu/simple/spring/mybatis/config/DataSourceConfig.java
@@ -7,7 +7,6 @@ import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -46,7 +45,6 @@ public class DataSourceConfig {
 
 
     @Bean
-    @Autowired
     public PlatformTransactionManager transactionManager(DataSource userDataSource) {
         return new DataSourceTransactionManager(userDataSource);
     }
